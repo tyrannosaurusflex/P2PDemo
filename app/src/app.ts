d397a66cfab8525d0c8c677d05c8e239a3ec4de7
Wajahat Akram <wajahatakram@wajahats-mbp.mynet>
2021-12-16 09:06:29 +0000

    clear solution

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,16 +1,1 @@
-import { start, dispatch } from 'nact';
-import { actr as InvestorSvc } from './services/investors';
-
-
-const system = start();
-const investorSvc = InvestorSvc(system);
-
-dispatch(investorSvc, 1);
-dispatch(investorSvc, 2);
-dispatch(investorSvc, 3);
-dispatch(investorSvc, 4);
-dispatch(investorSvc, 5);
-dispatch(investorSvc, 6);
-dispatch(investorSvc, 7);
-
-// from api / messages / somewhere else
+// the entry point for the app
